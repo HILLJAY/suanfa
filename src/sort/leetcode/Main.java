63f6fa9394d0e8fd665c3a1cbdaf493df3eed343
hilljay <782071950@qq.com>
2021-10-25 11:22:03 +0800

    second commit

--- src/sort/leetcode/Main.java
+++ src/sort/leetcode/Main.java
@@ -31,26 +31,7 @@ public class Main {
 
     }
 
-    public boolean isBalanced(TreeNode root) {
 
-        if(root == null) return true;
-        else {
-            int left = getHeight(root.left);
-            int right = getHeight(root.right);
-
-            return Math.abs(left - right) <= 1&&isBalanced(root.left)&&isBalanced(root.right);
-        }
-    }
-
-    public int getHeight(TreeNode root){
-
-        if(root == null) return 0;
-
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-
-        return Math.max(leftHeight,rightHeight)+1;
-    }
 }
 
 class ListNode {
